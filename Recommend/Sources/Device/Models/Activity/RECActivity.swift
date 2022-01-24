2366004340cbf6ae20d4abe1f718e886f7425ffa
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-01-24 04:43:15 +0200

    Update RECActivity

--- Recommend/Sources/Device/Models/Activity/RECActivity.swift
+++ Recommend/Sources/Device/Models/Activity/RECActivity.swift
@@ -9,7 +9,27 @@
 
 import Foundation
 
-public struct RECActivity<T>: Encodable {
-    public let type: String
-    public let data: T
+public enum RECActivity: Encodable {
+
+    // MARK: Coding Keys
+    
+    enum CodingKeys: String, CodingKey {
+        case type
+        case data
+    }
+    
+    // MARK: Encodable
+    
+    public func encode(to encoder: Encoder) throws {
+        let type: String!
+        var data: Data?
+        
+        //switch self {
+        //
+        //}
+        
+        var container = encoder.container(keyedBy: CodingKeys.self)
+        try container.encode(type, forKey: .type)
+        try container.encodeIfPresent(data, forKey: .data)
+    }
 }
