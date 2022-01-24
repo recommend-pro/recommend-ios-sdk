0e0f651bf91648b9446123d3ad835c847c61ab25
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-01-24 16:29:44 +0200

    Add RECRecommendation

---
+++ Recommend/Sources/Recommendation/RECRecommendation.swift
@@ -0,0 +1,19 @@
+//
+//  RECRecommendation.swift
+//  Recommend
+//
+//  Created by Dmytrii Golovanov on 08.12.2021.
+//  Copyright © 2022 Recommend OÜ. All rights reserved.
+//
+
+import Foundation
+
+final class RECRecommendation {
+    private let core: RECCore
+    
+    // MARK: Init
+    
+    init(core: RECCore) {
+        self.core = core
+    }
+}
