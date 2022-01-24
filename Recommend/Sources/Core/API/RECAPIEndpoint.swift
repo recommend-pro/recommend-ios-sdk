ca0679f0e2140a31af46ed2963a0d0e8612c8df4
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-01-24 02:27:01 +0200

    Replace queryItems, buildURL to RECAPIEndpoint

--- Recommend/Sources/Core/API/RECAPIEndpoint.swift
+++ Recommend/Sources/Core/API/RECAPIEndpoint.swift
@@ -10,13 +10,30 @@ import Foundation
 
 public struct RECAPIEndpoint {
     public let path: String
+    public var queryItems: [URLQueryItem]?
     public let httpMethod: String
     
     // MARK: Init
     
     public init(path: String,
+                queryItems: [URLQueryItem]? = nil,
                 httpMethod: String) {
         self.path = path
         self.httpMethod = httpMethod
+    }
+    
+    // MARK: URL
+    
+    func buildURL(host: String) throws -> URL {
+        var urlComponents = URLComponents()
+        urlComponents.scheme = "https"
+        urlComponents.host = host
+        urlComponents.path = path
+        urlComponents.queryItems = queryItems
+        
+        guard let url = urlComponents.url else {
+            throw RECAPIError.nilURL(urlComponents)
+        }
+        return url
     }
 }
--- Recommend/Sources/Core/API/RECAPIRequest.swift
+++ Recommend/Sources/Core/API/RECAPIRequest.swift
@@ -10,36 +10,20 @@ import Foundation
 
 public final class RECAPIRequest: NSObject {
     public var endpoint: RECAPIEndpoint
-    public var queryItems: [URLQueryItem]?
     public var headers: [String: String]?
     public var httpBody: Data?
     
     // MARK: Init
     
     public init(endpoint: RECAPIEndpoint,
-                queryItems: [URLQueryItem]? = nil,
-                httpBody: Data? = nil) {
         self.endpoint = endpoint
-        self.httpBody = httpBody
     }
+    
     
     // MARK: URLRequest
     
-    private func buildURL(host: String) throws -> URL {
-        var urlComponents = URLComponents()
-        urlComponents.scheme = "https"
-        urlComponents.host = host
-        urlComponents.path = endpoint.path
-        urlComponents.queryItems = queryItems
-        
-        guard let url = urlComponents.url else {
-            throw RECAPIError.nilURL(urlComponents)
-        }
-        return url
-    }
-    
     func buildURLRequest(host: String) throws -> URLRequest {
-        let url = try buildURL(host: host)
+        let url = try endpoint.buildURL(host: host)
         
         var urlRequest = URLRequest(url: url)
         urlRequest.httpMethod = endpoint.httpMethod
