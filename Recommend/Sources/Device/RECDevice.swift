af59ea7915cde89f250183f874f27b97c56405a9
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-01-24 03:56:20 +0200

    Add RECDevice

---
+++ Recommend/Sources/Device/RECDevice.swift
@@ -0,0 +1,19 @@
+//
+//  RECDevice.swift
+//  Recommend
+//
+//  Created by Dmytrii Golovanov on 07.12.2021.
+//  Copyright © 2022 Recommend OÜ. All rights reserved.
+//
+
+import Foundation
+
+final class RECDevice {
+    private let core: RECCore
+    
+    // MARK: Int
+    
+    init(core: RECCore) {
+        self.core = core
+    }
+}
