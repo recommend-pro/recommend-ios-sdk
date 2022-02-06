02d17a740f363f06eca2e62aa07cf41b0642cf40
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-02-07 00:55:26 +0200

    Add Date / Date list / Image / Image list

--- Recommend/Sources/Attribute/RECAttribute.swift
+++ Recommend/Sources/Attribute/RECAttribute.swift
@@ -11,43 +11,95 @@ import Foundation
 public enum RECAttribute: Decodable {
     case int(Int)
     case string(String)
-//    case date(Date)
-//    case image(URL)
+    case date(Date)
+    case image(URL)
     case float(Float)
     case bool(Bool)
     case intList([Int])
     case stringList([String])
-//    case dateList([Date])
-//    case imageList([URL])
+    case dateList([Date])
+    case imageList([URL])
     case floatList([Float])
+    
+    // MARK: Date Formatter
+    
+    private static lazy var dateFormatter: ISO8601DateFormatter = {
+        let formatter = ISO8601DateFormatter()
+        formatter.formatOptions = [.withFullDate,
+                                   .withDashSeparatorInDate,
+                                   .withTime,
+                                   .withColonSeparatorInTime]
+        return formatter
+    }()
+    
+    // MARK: Init
     
     public init(from decoder: Decoder) throws {
         let container = try decoder.singleValueContainer()
         
         if let value = try? container.decode(Int.self) {
             self = .int(value)
+            
         } else if let value = try? container.decode(String.self) {
-            self = .string(value)
-//        } else if let value = try? container.decode(Date.self) {
-//            self = .date(value)
-//        } else if let value = try? container.decode(URL.self) {
-//            self = .image(value)
+            self.init(from: value)
+            
         } else if let value = try? container.decode(Float.self) {
             self = .float(value)
+            
         } else if let value = try? container.decode(Bool.self) {
             self = .bool(value)
+            
         } else if let value = try? container.decode([Int].self) {
             self = .intList(value)
+            
         } else if let value = try? container.decode([String].self) {
-            self = .stringList(value)
-//        } else if let value = try? container.decode([Date].self) {
-//            self = .dateList(value)
-//        } else if let value = try? container.decode([URL].self) {
-//            self = .imageList(value)
+            self.init(from: value)
+            
         } else if let value = try? container.decode([Float].self) {
             self = .floatList(value)
+            
         } else {
             throw DecodingError.dataCorruptedError(in: container, debugDescription: "")
         }
+    }
+    
+    private init(from string: String) {
+        if let date = Self.dateFormatter.date(from: string) {
+            self = .date(date)
+        } else if let url = URL(string: string) {
+            self = .image(url)
+        } else {
+            self = .string(string)
+        }
+    }
+    
+    private init(from stringList: [String]) {
+        if let dateList = Self.dateList(from: stringList) {
+            self = .dateList(dateList)
+        } else if let imageList = Self.imageList(from: stringList) {
+            self = .imageList(imageList)
+        } else {
+            self = .stringList(stringList)
+        }
+    }
+    
+    // MARK: Date List
+    
+    private static func dateList(from stringList: [String]) -> [Date]? {
+        let dateList: [Date] = stringList.map({ Self.dateFormatter.date(from: $0) })
+        guard dateList.count == stringList.count else {
+            return nil
+        }
+        return dateList
+    }
+    
+    // MARK: Image (URL) List
+    
+    private static func imageList(from stringList: [String]) -> [Date]? {
+        let imageList: [URL] = stringList.map({ URL(string: $0) })
+        guard imageList.count == stringList.count else {
+            return nil
+        }
+        return imageList
     }
 }
