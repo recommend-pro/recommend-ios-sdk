370a9d6eb6bd35ac88f6bc531fd64e60b70c8552
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-02-08 03:00:03 +0200

    Add RECRecommendation fetchPanels logic

--- Recommend/Sources/Recommendation/RECRecommendation.swift
+++ Recommend/Sources/Recommendation/RECRecommendation.swift
@@ -10,6 +10,8 @@ import Foundation
 
 public final class RECRecommendation {
     private typealias APIService = RECRecommendationAPIService
+    public typealias PanelsFetchModel = RECRecommendationPanelsFetchModel
+    public typealias Panel = RECRecommendationPanel
     
     private let core: RECCore
     private let apiService: APIService
@@ -23,5 +25,49 @@ public final class RECRecommendation {
     init(core: RECCore) {
         self.core = core
         self.apiService = APIService(core: core)
+    }
+    
+    // MARK: Fetch Recommendation Panels
+    
+    private func fetchPanels(with model: PanelsFetchModel,
+                            completion: @escaping (Result<[Panel], Error>) -> Void) {
+        apiService.fetchPanels(model: model, completion: completion)
+    }
+    
+    public func fetchPanels(pageType: String? = nil,
+                            panels: [PanelsFetchModel.Panel]? = nil,
+                            previewPanel: PanelsFetchModel.PreviewPanel? = nil,
+                            completion: @escaping (Result<[Panel], Error>) -> Void) {
+        do {
+            let deviceId = try core.config.deviceId()
+            
+            guard let store = environment.store else {
+                throw RECRecommendationError.nilStore
+            }
+            
+            guard let currency = environment.currency else {
+                throw RECRecommendationError.nilCurrency
+            }
+            
+            guard let priceList = environment.priceList else {
+                throw RECRecommendationError.nilPriceList
+            }
+            
+            let model = PanelsFetchModel(deviceId: deviceId,
+                                         customerIdHash: environment.customerIdHash,
+                                         storeCode: store,
+                                         currencyCode: currency,
+                                         environmentCode: environment.environment,
+                                         priceList: priceList,
+                                         metrics: environment.metrics,
+                                         pageType: pageType,
+                                         contentType: .json,
+                                         panels: panels,
+                                         previewPanel: previewPanel)
+            self.fetchPanels(with: model, completion: completion)
+        } catch {
+            completion(.failure(error))
+            return
+        }
     }
 }
