d75492751536e79f74e258f2ac4c71c7f829e7d0
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-04-19 23:04:28 +0300

    Add main class base logic

--- Recommend/Sources/Recommend.swift
+++ Recommend/Sources/Recommend.swift
@@ -7,7 +7,43 @@
 //
 
 import Foundation
+import UIKit.UIApplication
 
 public final class Recommend: NSObject {
+    private let config: RECConfig
+    private let apiClient: RECAPIClient
     
+    public var deviceId: String? {
+        return try? config.getDeviceId()
+    }
+    
+    // MARK: Init
+    
+    public init(
+        accountId: String,
+        applicationName: String? = nil,
+        apiHost: String = kRECDefaultAPIHost
+    ) {
+        self.config = RECConfig(
+            accountId: accountId,
+            applicationName: applicationName,
+            apiHost: apiHost)
+        
+        self.apiClient = RECAPIClient(
+            host: apiHost,
+            urlSession: .shared)
+    }
+    
+    // MARK: Application
+
+    public func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) {
+        let isRemoteNotification = launchOptions?[.remoteNotification] as? [AnyHashable: Any] != nil
+        
+        if !(application.applicationState == .background && isRemoteNotification) {
+            self.config.appLaunched()
+        }
+    }
 }
