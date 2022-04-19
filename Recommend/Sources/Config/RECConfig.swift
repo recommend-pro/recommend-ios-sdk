6a2fa3b1a7958a09484f34d5fc0dbf9622b8901f
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-04-19 23:58:59 +0300

    Add Core logic

--- Recommend/Sources/Config/RECConfig.swift
+++ Recommend/Sources/Config/RECConfig.swift
@@ -11,90 +11,21 @@ import UIKit
 
 public let kRECDefaultAPIHost = "api.recommend.pro"
 
-// MARK: RECConfig
-
-public final class RECConfig {
-    
-    // MARK: UserDefauls
-    
-    private static let userDefaultsSuiteName = "pro.recommend.configuration"
-    private struct UserDefaultsKeys {
-        static let deviceId = "device_id"
-        static let firstLaunchDate = "first_launch_date"
-    }
-    
-    private lazy var userDefaults: UserDefaults! = {
-        return UserDefaults(suiteName: Self.userDefaultsSuiteName)
-    }()
-    
-    // MARK: Properties
+public struct RECConfig {
     
     public let accountId: String
     public let applicationName: String?
     public let apiHost: String
     
-    private var storedDeviceId: String? {
-        get {
-            userDefaults?.string(forKey: UserDefaultsKeys.deviceId)
-        }
-        set {
-            userDefaults?.set(newValue, forKey: UserDefaultsKeys.deviceId)
-        }
-    }
-    
-    public private(set) var firstLaunchDate: Date? {
-        get {
-            userDefaults?.value(forKey: UserDefaultsKeys.firstLaunchDate) as? Date
-        }
-        set {
-            userDefaults?.set(newValue, forKey: UserDefaultsKeys.firstLaunchDate)
-        }
-    }
-    
-    private var identifierForVendorUUIDString: String? {
-        return UIDevice.current.identifierForVendor?.uuidString
-    }
-    
     // MARK: Init
     
     init(
         accountId: String,
-        applicationName: String?,
+        applicationName: String? = nil,
         apiHost: String = kRECDefaultAPIHost
     ) {
         self.accountId = accountId
         self.applicationName = applicationName
         self.apiHost = apiHost
-    }
-    
-    // MARK: Device Id
-    
-    public func getDeviceId() throws -> String {
-        if let storedDeviceId = self.storedDeviceId {
-            if storedDeviceId.isEmpty == false {
-                return storedDeviceId
-            } else {
-                self.storedDeviceId = nil
-            }
-        }
-        
-        guard
-            let deviceId = self.identifierForVendorUUIDString,
-            deviceId.isEmpty == false
-        else {
-            throw RECConfigError.nilDeviceId
-        }
-        
-        self.storedDeviceId = deviceId
-        return deviceId
-    }
-    
-    // MARK: Open App
-    
-    func appLaunched() {
-        guard self.firstLaunchDate == nil else {
-            return
-        }
-        self.firstLaunchDate = Date()
     }
 }
