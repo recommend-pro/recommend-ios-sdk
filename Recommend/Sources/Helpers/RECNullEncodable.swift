acf9f7e91fdf49ae59051fee0a8bd3ffe1b7bea3
Dmytrii Golovanov <dmytrii.golovanov@gmail.com>
2022-04-20 00:20:50 +0300

    Add RECNullEncodable propertyWrapper

---
+++ Recommend/Sources/Helpers/RECNullEncodable.swift
@@ -0,0 +1,29 @@
+//
+//  RECNullEncodable.swift
+//  Recommend
+//
+//  Created by Dmytrii Golovanov on 08.12.2021.
+//  Copyright © 2022 Recommend OÜ. All rights reserved.
+//
+
+import Foundation
+
+@propertyWrapper
+public struct RECNullEncodable<T>: Encodable where T: Encodable {
+    public var wrappedValue: T?
+    
+    public init(wrappedValue: T?) {
+        self.wrappedValue = wrappedValue
+    }
+    
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.singleValueContainer()
+        
+        switch wrappedValue {
+        case .some(let value):
+            try container.encode(value)
+        case .none:
+            try container.encodeNil()
+        }
+    }
+}
