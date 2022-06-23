d44e6b96ad34d4f5e2e57e0a82eda3a7813798bb
Dmytrii Golovanov <dimag@ronis.mail>
2022-06-23 19:51:40 +0300

    Add xcodeproj

---
+++ Recommend/Sources/Recommend.h
@@ -0,0 +1,18 @@
+//
+//  Recommend.h
+//  Recommend
+//
+//  Created by Dmytrii Golovanov on 23.06.2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Recommend.
+FOUNDATION_EXPORT double RecommendVersionNumber;
+
+//! Project version string for Recommend.
+FOUNDATION_EXPORT const unsigned char RecommendVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Recommend/PublicHeader.h>
+
+
