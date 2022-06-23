74aacad4bdc5e6e2a551ca4da204a27eaad2cd3b
Dmytrii <dmytrii.golovanov@gmail.com>
2022-06-23 21:29:12 +0300

    Add Demo project

---
+++ Demo/Demo/Application/AppDelegate.swift
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.swift
+//  Demo
+//
+//  Created by Dmytrii Golovanov on 23.06.2022.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+}
+
---
+++ Demo/Demo/Presentation/ViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by Dmytrii Golovanov on 23.06.2022.
+//  Copyright © 2022 Recommend OÜ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
