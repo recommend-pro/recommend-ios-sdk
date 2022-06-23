ecb3605b9a9c16d07001e969784c190a0c39f588
Dmytrii <dmytrii.golovanov@gmail.com>
2022-06-23 21:38:32 +0300

    Add Messaging integration

--- Demo/Demo/Application/AppDelegate.swift
+++ Demo/Demo/Application/AppDelegate.swift
@@ -3,17 +3,59 @@
 //  Demo
 //
 //  Created by Dmytrii Golovanov on 23.06.2022.
+//  Copyright © 2022 Recommend OÜ. All rights reserved.
 //
 
 import UIKit
+import Recommend
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        
+        Recommend.initialize(
+            accountId: <#Account ID#>,
+            applicationName: <#Push Integration Name#>
+        )
+        
+        Recommend.shared.application(
+            application,
+            didFinishLaunchingWithOptions: launchOptions
+        )
+        
+        UIApplication.shared.registerForRemoteNotifications()
+        
         return true
+    }
+    
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        Recommend.shared.applicationWillEnterForeground()
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didRegisterForRemoteNotificationsWithDeviceToken deviceToken: Data
+    ) {
+        Recommend.shared.setDeviceToken(deviceToken)
+    }
+    
+    func application(
+        _ application: UIApplication,
+        didReceiveRemoteNotification userInfo: [AnyHashable : Any],
+        fetchCompletionHandler completionHandler: @escaping (UIBackgroundFetchResult) -> Void) {
+            
+        Recommend.shared.applicationDidReceiveRemoteNotification(userInfo)
+        if let url = Recommend.shared.remoteNotificationOpenURL(userInfo) {
+            print("Recommend URL: \(url.absoluteString)")
+        }
+            
+        completionHandler(.newData)
     }
 }
 
