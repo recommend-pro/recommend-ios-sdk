c9145171178bfda9971e1cfd244b7b835d98ee47
Pavel Tsvihun <pavel.tsvihun@ronis.info>
2023-12-14 09:21:09 +0200

    Added thread safety in RECAPIQueue

--- Recommend/Sources/API/Queue/RECAPIQueue.swift
+++ Recommend/Sources/API/Queue/RECAPIQueue.swift
@@ -11,16 +11,24 @@ import Foundation
 final class RECAPIQueue {
     
     private var queue: Set<RECAPIQueueItem> = []
+    private let lock = NSLock()
     
     // MARK: Add Item
     
     func add(_ item: RECAPIQueueItem) {
+        self.lock.lock()
+        defer { self.lock.unlock() }
+        
         queue.insert(item)
     }
     
     // MARK: Next Item
     
     func next() -> RECAPIQueueItem? {
+        
+        self.lock.lock()
+        defer { self.lock.unlock() }
+        
         guard queue.count > 0 else {
             return nil
         }
